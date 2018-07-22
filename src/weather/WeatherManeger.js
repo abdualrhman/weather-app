9862b8ec6f231ad1ecbcc10d92a3bb01721cbf3c
Abdulrhman Ramadan <abdalrhman07094h@gmail.com>
2018-07-22 13:55:01 +0200

    add an error handler

--- src/weather/WeatherManeger.js
+++ src/weather/WeatherManeger.js
@@ -4,7 +4,7 @@ import WeatherList from './WeatherList.js'
 
 
 /*
-full url
+full api url
 http://api.openweathermap.org/data/2.5/weather?q=copenhagen&appid=a5a8f2f0a0571c3d425f6ad15260911c&units=metric
 */
 const newUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
@@ -16,6 +16,7 @@ export default class WeatherManeger extends React.Component {
       valueList : null,
       cityTarget : 'copenhagen',
       loading : true,
+      error:false,
       main: {},
     }
     this.fetchData = this.fetchData.bind(this)
@@ -27,18 +28,31 @@ componentDidMount(){
 }
 
   fetchData(){
+    const me=this;
     this.setState({
       loading : true
     })
     const {cityTarget} = this.state;
     fetch(`${newUrl}${cityTarget}${apiKey}`)
-    .then((res)=>
-          res.json()
-    )
-    .then(data => {
-      this.setState({ valueList : data, loading : false, main : data.main }, ()=>{console.log(this.state.valueList)})
-    })
+    .then(function(response) {
+  if(response.ok) {
+    return response.json();
+  }
+  // throw new Error('Network response was not ok.');
 
+  me.setState({
+    error : true
+  }, ()=>{console.log(me.state.error+'erererererererer')})
+})
+    // .then((res)=>
+    //   console.log(res)
+    // )
+    .then(data => {
+      this.setState({ valueList : data, loading : false, main : data.main}, ()=>{console.log(this.state.valueList)})
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   }
   changeFunc(event){
     const {value} = event.target;
@@ -59,7 +73,6 @@ componentDidMount(){
           changeHandler={this.changeFunc}
           city={this.state.cityTarget}
           />
-
           <WeatherList list={this.state.valueList}
           loading={this.state.loading}
           main={this.state.main}
@@ -67,7 +80,10 @@ componentDidMount(){
           lng:this.state.valueList.coord.lon}}
           />
         </div>
-      }</div>
+      }
+      {this.state.error && <h4 className="error">Please enter a valid city name</h4>}
+      </div>
+
     );
   }
 }
